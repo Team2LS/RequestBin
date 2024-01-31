bb28fcf454512623dac5d6efcd9084eb392b8ed7
emargetis <erik.margetis@gmail.com>
2024-01-31 09:27:52 -0600

    adding files to be delted

--- request-bin-backend/build/requestSchema.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const mongoose = require('mongoose');
-const payloadSchema = new mongoose.Schema({ payloadData: Buffer });
-const Payload = mongoose.model('payload', payloadSchema);
-module.exports = { Payload };
--- request-bin-backend/src/requestSchema.ts
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const payloadSchema = new mongoose.Schema({ payloadData: Buffer });
-const Payload = mongoose.model('payload', payloadSchema);
-
-module.exports = {Payload}
