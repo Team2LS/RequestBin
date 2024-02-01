eeb2826ccaebb37890c099538934165210a8e184
emargetis <erik.margetis@gmail.com>
2024-01-31 20:59:33 -0600

    updated payload endpoint to return headers

--- request-bin-backend/src/services/mongodb.ts
+++ request-bin-backend/src/services/mongodb.ts
@@ -1,12 +1,13 @@
 import * as mongoose from 'mongoose';
 
-const payloadSchema = new mongoose.Schema({ payloadData: {} });
+const payloadSchema = new mongoose.Schema({ payloadData: {}, headers: {} });
 const Payload = mongoose.model('Payload', payloadSchema);
 
 mongoose.connect('mongodb://127.0.0.1/rhh')
 const db = mongoose.connection
 
 async function savePayload(req: any) {
+  console.log(req.headers);
   const payload = new Payload({"payloadData": req.body, "headers": req.headers})
 
   return payload.save().then((result) => {
@@ -17,9 +18,13 @@ async function savePayload(req: any) {
 
 async function getPayloadById(id: string) {
   const result = await Payload.findById(id)
+  console.log(result)
   if (result === null) { return null }
 
-  return result.payloadData
+  return {
+    headers: result.headers,
+    payload: result.payloadData
+  }
 }
 
 async function getAllPayloads() {
