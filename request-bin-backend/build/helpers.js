abb53684f2eb8490b875d489bad17f8f5c544cf0
Franco Waisfeld <francowaisfeld@francos-mbp-3.home>
2024-02-01 20:23:00 -0800

    changed helper.ts makeUrlPath function to only have lowercase letters

--- request-bin-backend/build/helpers.js
+++ request-bin-backend/build/helpers.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.makeUrlPath = void 0;
 function makeUrlPath(length) {
     let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
     const charactersLength = characters.length;
     let counter = 0;
     while (counter < length) {
